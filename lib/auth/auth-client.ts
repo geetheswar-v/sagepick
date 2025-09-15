38638a8eb71ce8a2cbc1309c4f5dc02cbcc1c0ac
geetheswar-v <geetheswar.edu@gmail.com>
2025-09-16 02:18:29 +0530

    actions: revert back to preview CD and updated base url

--- lib/auth/auth-client.ts
+++ lib/auth/auth-client.ts
@@ -2,10 +2,7 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   baseURL:
-    process.env.BETTER_AUTH_URL ||
-    (typeof window !== "undefined"
-      ? window.location.origin
-      : "http://localhost:3000"),
+    process.env.BETTER_AUTH_URL || "https://sagepick.vercel.app",
   fetchOptions: {
     credentials: "include",
   },
--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -4,6 +4,12 @@ import { prisma } from "../prisma";
 import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL || "https://sagepick.vercel.app",
+  trustedOrigins: [
+    "http://localhost:3000",
+    "https://sagepick.vercel.app",
+    ...(process.env.VERCEL_URL ? [`https://${process.env.VERCEL_URL}`] : []),
+  ],
   emailAndPassword: {
     enabled: true,
   },
